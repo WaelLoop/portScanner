7012171c0f8ba23d84cf9519a6d4f8d83e72333d
Wael Alhashemi <alhashemiw@gmail.com>
2019-11-19 13:56:36 -0500

    Added comments

--- portScanner.py
+++ portScanner.py
@@ -2,18 +2,22 @@ import sys
 import nmap
 import datetime
 
-
+# function that does the port scan using nmap library
 def portScanning(ip):
     try:
+	#create our output file
         file = open(str(datetime.datetime.now()), 'a+')
+	#initialize our nmap scanner object
         nm = nmap.PortScanner()
+	#nmap -oX - -p 22-443 ip
         nm.scan(ip,'22-443')
-
+	# appending hostname and if it's status
         file.write("="*50 + '\n')
         file.write('Host: %s\n' % ip)
         file.write('Hostname: %s\n' % nm[ip].hostname())
         file.write('State: %s\n' % nm[ip].state())
 
+	# append protocols and ports
         for protocol in nm[ip].all_protocols():
             file.write('=' * 15 + '\n')
             file.write('Protocol: %s\n' % protocol)
@@ -23,9 +27,10 @@ def portScanning(ip):
             for port in listOfPorts:
                 file.write('Port: %s  State: %s\n' % (port, nm[ip][protocol][port]['state']))
     except Exception as e:
+	# error handling
         print("Couldnt Scan" + ip + ": " + e)
 
-
+# method to read the file of hosts to scan
 def readHostsFromFile():
     hosts= open(sys.argv[1], 'r')
     for host in hosts.readlines():
