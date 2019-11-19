881a94b53e8d041681c20f87a4426fe5a42145a5
waelloop <alhashemiw@gmail.com>
2019-11-19 13:49:04 -0500

    added some error handling

--- portScanner.py
+++ portScanner.py
@@ -4,30 +4,36 @@ import datetime
 
 
 def portScanning(ip):
-    file = open(str(datetime.datetime.now()), 'w+')
-    nm = nmap.PortScanner()
-    nm.scan(ip,'22-443')
+    try:
+        file = open(str(datetime.datetime.now()), 'a+')
+        nm = nmap.PortScanner()
+        nm.scan(ip,'22-443')
 
-    file.write("="*50 + '\n')
-    file.write('Host: %s\n' % ip)
-    file.write('Hostname: %s\n' % nm[ip].hostname())
-    file.write('State: %s\n' % nm[ip].state())
+        file.write("="*50 + '\n')
+        file.write('Host: %s\n' % ip)
+        file.write('Hostname: %s\n' % nm[ip].hostname())
+        file.write('State: %s\n' % nm[ip].state())
 
-    for protocol in nm[ip].all_protocols():
-        file.write('=' * 15 + '\n')
-        file.write('Protocol: %s\n' % protocol)
+        for protocol in nm[ip].all_protocols():
+            file.write('=' * 15 + '\n')
+            file.write('Protocol: %s\n' % protocol)
 
-        listOfPorts = nm[ip][protocol].keys()
+            listOfPorts = nm[ip][protocol].keys()
 
-        for port in listOfPorts:
-            file.write('Port: %s  State: %s\n' % (port, nm[ip][protocol][port]['state']))
+            for port in listOfPorts:
+                file.write('Port: %s  State: %s\n' % (port, nm[ip][protocol][port]['state']))
+    except Exception as e:
+        print("Couldnt Scan" + ip + ": " + e)
 
 
 def readHostsFromFile():
-    hosts= open(sys.argv[1], 'w')
-    for host in hosts.readline():
-        portScanning(host)
+    hosts= open(sys.argv[1], 'r')
+    for host in hosts.readlines():
+        portScanning(host.strip())
 
 
 if __name__ == '__main__':
-    readHostsFromFile()
+	if(len(sys.argv) == 2):
+		readHostsFromFile()
+	else:
+		print("invalid input. Pass in hosts.txt")
